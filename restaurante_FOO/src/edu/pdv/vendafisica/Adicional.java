317163f93e38b84cf36a2da34c6be329f3de1008
AgathaLice <miguelfabrisd@gmail.com>
2024-11-29 18:06:56 -0300

    Update de nome de Adicional
    O nome da classe e construtor estavam como AdicionalItemPedido, modifiquei a Adicional.

--- restaurante_FOO/src/edu/pdv/vendafisica/Adicional.java
+++ restaurante_FOO/src/edu/pdv/vendafisica/Adicional.java
@@ -1,6 +1,6 @@
 package edu.pdv.vendafisica;
 
-public class AdicionalItemPedido {
+public class Adicional {
     private long id;
     private String nome;
     private double preco;
@@ -8,7 +8,7 @@ public class AdicionalItemPedido {
     private double desconto;
 
     //Contrutor, não passei o ID pois ele é autogerado (eu acho)
-    public AdicionalItemPedido(String nome, double preco, int quantidade) {
+    public Adicional(String nome, double preco, int quantidade) {
         this.nome = nome;
         this.preco = preco;
         this.quantidade = quantidade;
