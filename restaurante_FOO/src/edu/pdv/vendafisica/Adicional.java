5f08cdde56d5797aaa29262c433a60007007d2fd
AgathaLice <miguelfabrisd@gmail.com>
2024-11-29 18:03:48 -0300

    Update em Adicional
    Modifiquei o nome de AdicionalItemPedido para Adicional; criei os métodos e adicionei o atributo desconto, que não existia antes.

---
+++ restaurante_FOO/src/edu/pdv/vendafisica/Adicional.java
@@ -0,0 +1,31 @@
+package edu.pdv.vendafisica;
+
+public class AdicionalItemPedido {
+    private long id;
+    private String nome;
+    private double preco;
+    private int quantidade;
+    private double desconto;
+
+    //Contrutor, não passei o ID pois ele é autogerado (eu acho)
+    public AdicionalItemPedido(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+    // Getters
+    public long getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+
+    // Setters
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
+    }
+}
