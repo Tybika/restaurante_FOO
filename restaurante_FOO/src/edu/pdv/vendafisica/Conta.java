f75457f763ad23d5537bddd729a2eccdb0ac86e3
Yasmim <108767728+Tybika@users.noreply.github.com>
2024-11-29 23:05:25 -0300

    Update Conta.java

--- restaurante_FOO/src/edu/pdv/vendafisica/Conta.java
+++ restaurante_FOO/src/edu/pdv/vendafisica/Conta.java
@@ -1,20 +1,25 @@
 package edu.pdv.vendafisica;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Calendar;
+
 public class Conta {
     private Pedido pedido;
     private float valorTotal;
     private float totalDesconto;
     private float valorRestante;
 
+    // Construtor da classe
     public Conta(Pedido pedido){
         this.pedido = pedido;
         this.valorTotal = this.calcularValorTotal();
         this.totalDesconto = this.calcularDescontoPercentual() +
                              this.calcularDescontoNominal() + this.calcularDescontoProgressivo();
-        //Tem que inserir o desconto que venha de itens aqui tbm (ou fazê-lo em valorTotal)
         this.valorRestante = valorTotal - totalDesconto;
     }
 
+    // Calcula valor total
     private double calcularValorTotal(){
         List<ItemPedido> itens = this.pedido.getItems();
         int[] quantidade = this.pedido.getQuantidade();
@@ -22,11 +27,13 @@ public class Conta {
 
         for (int i = 0; i < itens.size(); i++){
             valorTotal += itens.get(i).getPreco() * quantidade[i];
+            valorTotal = valorTotal - itens.get(i).getDesconto() * quantidade[i];
         }
 
         return valorTotal;
     }
 
+    // Calcula descontos percentuais
     public double calcularDescontoPercentual(double n){
         if (n <= 0 || n > 100){
             return 0;
@@ -40,6 +47,7 @@ public class Conta {
         return desconto;
     }
 
+    // Calcula descontos nominais
     public double calcularDescontoNominal(double desconto){
         if (n <= 0 || n > valorTotal){
             return 0;
@@ -48,9 +56,10 @@ public class Conta {
         return desconto;
     }
 
+    // Calcula desconto progressivo
     public double calcularDescontoProgressivo(long itemId, double descontoMin,
                                               double descontoMax, double progressao, int qntMin){
-        //Verifica validade dos parâmetros
+        // Verifica validade dos parâmetros
         if (descontoMax > descontoMin && descontoMax <= 100 && descontoMin > 0 &&
                 progressao > 0 && progressao <= descontoMax && qntMin > 0){
             double preco;
@@ -61,36 +70,111 @@ public class Conta {
             int[] quantidade = this.pedido.getQuantidade;
             int qnt = 0;
 
-            //Itera itens buscando itemId
+            // Itera itens buscando itemId
             for (int i = 0; i < itens.size(); i++){
-                n = itens.get(i).getId;
-                if (n == itemId){
-                    preco = itens.get(i).getPreco;
+                n = itens.get(i).getId();
+                if(n == itemId){
+                    preco = itens.get(i).getPreco();
                     qnt = quantidade[i];
                     break;
                 }
             }
 
-            //Calcula se
+            // Verifica quantidade de itens
             if (qnt > qntMin){
                 int dif, intervalos, qntMax;
-                double desconto;
+                double desconto = 0;
 
-                for (int i = qntMin; i < qnt; i++){
-                    if (i < qntMax){
-
+                // Aumenta a progressão para cada número de diferença entre qnt e min
+                for(int i = qntMin; i < qnt; i++){
+                    if ((desconto + progressao) <= descontoMax ){
+                        desconto += progressao;
+                    }
+                    else{
+                        break;
                     }
                 }
 
-                dif = qnt - qntMin;
-                intervalos = (int) descontoMax / progressao;
-                qntMax = qntMin + intervalos;
+                if (desconto > 1){
+                    desconto = desconto / 100;
+                }
 
-                desconto = (preco* ) * qnt;
+                desconto = (desconto * preco) * qnt;
                 return desconto;
             }
         }
         return 0;
+    }
+
+    // Calcula o valor da divisão em partes iguais
+    public double dividirIgual(int divisor){
+        double parcela;
+
+        parcela = valorRestante / divisor;
+        return parcela;
+    }
+
+    // Calcula o valor de itens da conta
+    public double dividirItem(long itemId, int quantidade) {
+        if (quantidade <= 0){
+            return 0;
+        }
+
+        double parcela;
+        int n;
+        List<ItemPedido> itens = this.pedido.getItens();
+        int[] quantidade = this.pedido.getQuantidade();
+        int qnt = 0;
+
+        for (int i = 0; i < itens.size(); i++){
+            n = itens.get(i).getId();
+            if (n == itemId) {
+                preco = itens.get(i).getPreco();
+                qnt = quantidade[i];
+                break;
+            }
+        }
+
+        if (qnt <= quantidade){
+            parcela = preco * quantidade;
+            return parcela;
+        } else {
+            return 0;
+        }
 
     }
+
+    // Deduz do valorRestante
+    public void pagar(double valor){
+        if (valor <= 0 || valor > this.valorRestante){
+            return;
+        }
+
+        pagamento = new Pagamento(valor);
+        this.setValorRestante((this.valorRestante - valor));
+        return;
+    }
+
+    // Getters
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public double getTotalDesconto() {
+        return totalDesconto;
+    }
+
+    public double getValorRestante() {
+        return valorRestante;
+    }
+
+    // Setters
+    public void setValorRestante(double valor) {
+        this.valorRestante = valor;
+    }
+
 }
