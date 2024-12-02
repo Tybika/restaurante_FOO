3e70ffa22ffd335ea9208bb90ed2b8f3169e79a6
Yasmim <108767728+Tybika@users.noreply.github.com>
2024-12-02 10:29:13 -0300

    Update Pedido.java
    correção tipo dado e mudanças no construtor e setters

--- restaurante_FOO/src/edu/pdv/vendafisica/Pedido.java
+++ restaurante_FOO/src/edu/pdv/vendafisica/Pedido.java
@@ -1,54 +1,89 @@
 package edu.pdv.vendafisica;
 
-import java.util.*
+import java.util.*;
 
 public class Pedido {
     private long id;
     private String mesa;
-    private Calendar data;
-    private Calendar horaAbertura;
-    private Calendar horaFechamento;
+    private Date data;
+    private Date horaAbertura;
+    private Date horaFechamento;
     private boolean isAberto;
     private Atendente atendente;
-    private List<ItemPedido> items;
-    private int[] quantidade; //precisa repensar o tipo desse atributo
-
-    public Pedido(String mesa, Atendente atendente) {
-
-    }
     private List<ItemPedido> itens;
     private int[] quantidade;
 
-    // Construtor
-    // Não boto nem 1% de confiança no que eu escrevi...
-    public Pedido(String mesa, Date data, Date horaAbertura, Date horaFechamento,
-                  boolean isAberto, Atendente atendente, List<ItemPedido> itens, int[] quantidade){
+    // Construtores
+    public Pedido(String mesa, Atendente atendente,
+                  List<ItemPedido> itens, int quantidade){
+        Calendar calendar = Calendar.getInstance();
+
         this.mesa = mesa;
-        this.data = data;
-        this.horaAbertura = horaAbertura;
-        this.horaFechamento = horaFechamento;
-        this.isAberto = isAberto;
+        this.data = calendar.date(); // conferir met
+        this.horaAbertura = calendar.getTime();
+        this.horaFechamento = 0;
+        this.isAberto = true;
         this.atendente = atendente;
-        this.itens = new List<itens>;
-        this.quantidade = quantidade;
+        this.itens = new List<ItemPedido>;
+        this.quantidade[buscarUltimo()] = quantidade;
     }
 
-    // Métodos normais
-
+    public int buscarUltimo(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                return i;
+            }
+        }
+    }
 
     // Getters
-    public long getId() { return id; }
-    public String getMesa() { return mesa; }
-    public Date getData() { return data; }
-    public Date getHoraAbertura() { return horaAbertura; }
-    public Date getHoraFechamento() { return horaFechamento; }
-    public boolean getIsAberto() { return isAberto; }
-    public Atendente getAtendente() { return atendente; }
-    public List<ItemPedido> getItens() { return itens; }
-    public int[] getQuantidade() { return quantidade; }
+    public long getId() {
+        return id;
+    }
+
+    public String getMesa() {
+        return mesa;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public Date getHoraAbertura() {
+        return horaAbertura;
+    }
+
+    public Date getHoraFechamento() {
+        return horaFechamento;
+    }
+
+    public boolean getIsAberto() {
+        return isAberto;
+    }
+
+    public Atendente getAtendente() {
+        return atendente;
+    }
+
+    public List<ItemPedido> getItens() {
+        return List<ItemPedido>;
+    }
+
+    public int[] getQuantidade() {
+        return quantidade;
+    }
 
     // Setters
-    public void setIsAberto(){ this.isAberto = isAberto; }
-    public void setHoraFechamento(){ this.horaFechamento = horaFechamento; }
+    public void setIsAberto(boolean estado){
+        this.isAberto = estado;
+        if (estado == false) {
+            Calendar aleatorio = Calendar.getInstance();
+            setHoraFechamento(calendar.getTime());
+        }
+    }
+
+    public void setHoraFechamento(Date hora){
+        this.horaFechamento = hora;
+    }
 
 }
